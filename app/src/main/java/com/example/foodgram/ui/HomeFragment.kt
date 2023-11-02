3deace8bb0646646bfea94a8ee4c57cb8f6d6372
Muslima Karimova <muslima@Muslimas-MacBook-Pro.local>
2023-11-02 15:45:08 +0000

    error free UI

---
+++ app/src/main/java/com/example/foodgram/ui/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.foodgram.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.foodgram.databinding.FragmentHomeBinding
+import com.example.foodgram.databinding.FragmentProfileBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
--- app/src/main/java/com/example/foodgram/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/foodgram/ui/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.example.foodgram.ui.profile
+package com.example.foodgram.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,36 +7,25 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.foodgram.databinding.FragmentHomeBinding
 import com.example.foodgram.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val profileViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
-
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
     }
 }
