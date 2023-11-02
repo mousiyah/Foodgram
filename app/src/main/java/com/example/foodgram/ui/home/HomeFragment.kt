252b61287c938bd217f3808c8d93ddb055362209
Muslima Karimova <muslima@Muslimas-MacBook-Pro.local>
2023-11-02 14:33:28 +0000

    ui

--- app/src/main/java/com/example/foodgram/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/foodgram/ui/home/HomeFragment.kt
@@ -1,40 +1,43 @@
-package com.example.foodgram.ui.home
+package com.example.foodgram.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.foodgram.R
-import com.example.foodgram.ui.home.adapter.PostAdapter
+import androidx.lifecycle.ViewModelProvider
+import com.example.foodgram.databinding.FragmentProfileBinding
+import com.example.foodgram.ui.home.HomeViewModel
 
 class HomeFragment : Fragment() {
-    private lateinit var postAdapter: PostAdapter
 
-    // Sample data for posts
-    private val samplePosts = listOf(
-        PostModel("Post 1", "Description for Post 1", R.drawable.ic_home),
-        PostModel("Post 2", "Description for Post 2", R.drawable.ic_home),
-        // Add more sample posts here
-    )
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        // Initialize RecyclerView
-        val recyclerView: RecyclerView = view.findViewById(R.id.postRecyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(context)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
-        // Create and set the adapter
-        postAdapter = PostAdapter(samplePosts)
-        recyclerView.adapter = postAdapter
+        val textView: TextView = binding.textNotifications
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
 
-        return view
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/foodgram/ui/home/PostModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.foodgram.ui.home
-
-data class PostModel(
-    val title: String,
-    val description: String,
-    val imageResource: Int // Resource ID for the post image
-)
