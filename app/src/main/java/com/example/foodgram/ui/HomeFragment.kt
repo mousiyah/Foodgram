cee75a3e1ab0206b8bd68b14898ef771b1c333ec
Muslima Karimova <muslima@Muslimas-MacBook-Pro.local>
2023-11-05 23:02:03 +0000

    ready

--- app/src/main/java/com/example/foodgram/ui/HomeFragment.kt
+++ app/src/main/java/com/example/foodgram/ui/HomeFragment.kt
@@ -5,23 +5,67 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.foodgram.databinding.FragmentHomeBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodgram.R
 
 class HomeFragment : Fragment() {
 
-    private lateinit var binding: FragmentHomeBinding
+    private lateinit var reviewsRecyclerView: RecyclerView
+    private lateinit var reviewAdapter: ReviewAdapter
+    private lateinit var reviewList: List<ReviewItem>
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        reviewsRecyclerView = root.findViewById(R.id.reviewsRecyclerView)
+        reviewsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        populateReviews()
+            reviewAdapter = ReviewAdapter(requireContext(), reviewList)
+        reviewsRecyclerView.adapter = reviewAdapter
 
         return root
     }
+
+    private fun populateReviews(){
+        reviewList = listOf(
+            ReviewItem(
+                username = "azra.lin",
+                location = "Sushi bar & restaurant, Swansea",
+                tags = "#sushi",
+                description = "The best sushi place I have ever been to. Good for family gatherings. Portions are good size and they served us free tea 🍣☕️ Definitely will come back again",
+                starRating = 4.5f, // 4.5 stars
+                timeAgo = "1h ago",
+                imageList = listOf(
+                    R.drawable.p1, // Sample image resource IDs
+                    R.drawable.p2,
+                    R.drawable.p3
+                )
+            ),
+            ReviewItem(
+                username = "azra.lin",
+                location = "Sushi bar & restaurant, Swansea",
+                tags = "#sushi",
+                description = "The best sushi place I have ever been to. Good for family gatherings. Portions are good size and they served us free tea 🍣☕️ Definitely will come back again",
+                starRating = 4.5f, // 4.5 stars
+                timeAgo = "1h ago",
+                imageList = listOf(
+                    R.drawable.p1, // Sample image resource IDs
+                    R.drawable.p2,
+                    R.drawable.p3
+                )
+            ),
+            // Add more ReviewItem objects as needed
+        )
+
+    }
+
 
 
     override fun onDestroyView() {
